8a4d1d80cb873e01fb722f47a9d0a8e3b808cc80
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-10-08 17:07:39 +0300

    (4 section side menu & responsive)

--- js/min.js
+++ js/min.js
@@ -14,4 +14,27 @@ $(document).ready(function(){
         animateOut:'fadeOut',
         smartSpeed:450
     })
+
+    //side-menu
+    $('.menu').on('click',function(){
+        $('.side-menu').toggleClass('open')
+    })
+
+    $('.fa-times').on('click',function(){
+        $('.side-menu').removeClass('open')
+    })
+
+
+    //toggle nav
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>= 30){
+            $('.nav-left').fadeOut(500);
+            $('.nav-top').fadeIn(500);
+        }
+        else{
+            $('.nav-left').fadeIn(500);
+            $('.nav-top').fadeOut(500);
+        }
+    })
 })
